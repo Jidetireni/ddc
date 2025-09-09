17e0f07d41a30d2aad1f1bc6be0fbb5d59f2561c
Jidetireni <jtirenipraise@gmail.com>
2025-09-09 14:11:18 +0100

    Add detailed comments to MinOperations function for better understanding of the algorithm

--- challenges/greedy/1827. Minimum Operations to Make the Array Increasing.go
+++ challenges/greedy/1827. Minimum Operations to Make the Array Increasing.go
@@ -1,5 +1,18 @@
 package greedy
 
+/*
+	We are asked to return the number of operations needed to make the array
+	strictly increasing, i.e., nums[i] < nums[i+1].
+
+	My thought process was to act greedily: increase each element only when necessary,
+	by at most +1 from the previous element. I calculate the required value (+1 of the
+	previous element), then compare it with the current element.
+
+	If the required value is greater, I find the number of operations by subtracting
+	the current value from the required value. Then I update the current element to
+	that required value.
+*/
+
 func MinOperations(nums []int) int {
 	operations := 0
 	for i := 1; i < len(nums); i++ {
